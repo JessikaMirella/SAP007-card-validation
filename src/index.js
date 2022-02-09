f5b06120ec07f21414906adcb7830068a6239b48
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-02-09 14:16:53 -0300

    quase finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
 
-console.log(validator);
+console.log(validator)
+
+
+
+let botaoConfirma = document.getElementById("botao");
+
+
+botaoConfirma.addEventListener("click", function () {
+    let cardNumber = document.getElementById("numeroCartao").value;
+    let resultado = validator.isValid(cardNumber)
+    
+    console.log("clicou!", cardNumber, resultado)
+
+});
+
+botaoConfirma.addEventListener("click", function () {
+    
+    if (document.getElementById("numeroCartao").value.length < 3) {
+        alert('Por favor, preencha todos os campos');
+        document.getElementById("numeroCartao").focus();
+
+        return false
+
+
+}});
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,190 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+
+const validator = {
+   
+  isValid: 
+
+  function (cardNumberString) { 
+
+
+   console.log(cardNumberString, "string")
+    let cardNumber = cardNumberString.split("")
+    console.log(cardNumber, "array?")
+
+    cardNumber.reverse();
+    console.log (cardNumber, "inverteu?")
+    let dobra = 0
+    let sub = 0
+    let total = 0
+    
+   
+  
+  for (let i = 0; i < cardNumber.length; i++) {
+  
+      if (i % 2 === 0) {
+  
+          total = total + cardNumber[i]
+          console.log("entrou no if",i, cardNumber[i], "essa posição é impar" )
+  
+  
+      } else {
+          dobra = cardNumber[i] * 2;
+          console.log("entrou no else",i, cardNumber[i], "essa posição é par", dobra )
+          if (dobra >= 10) {
+              sub = dobra - 9
+          console.log(" o dobro é maior que 9")
+              total = total + sub
+          }
+  
+          else {
+              total = total + dobra
+          
+  
+  
+          } if (total % 10 === 0 ) { 
+            alert("Tudo Ok! Podemos prosseguir!") 
+           return true
+           
+           
+    
+          } else{ 
+            alert("Cartão Invalido ou Inexistente! Preencha corretamente!")
+
+            return false
+            
+            
+        
+  
+          }
+  
+  
+      }
+    }
+  }
+
+    
+
+
+    
+    
+  
+
+
+
+
+};
+  
+//  function mascarar (cardNumberT){
+//  if (cardNumberT.length < 6) return cardNumbeT;
+ // const last4Characters = cardNumberT.substr(-4);
+ // const firstCharacter = cardNumberT.substr(0, 0);
+  //const maskingCharacters = cardNumberT.substr(1, cardNumberT.length - 5).replace(/\d/g, '#');
+  //return `${firstCharacter}${maskingCharacters}${last4Characters}`;
+
+//};
+
+//console.log(mascarar)(cardNumber)
+  
+
+
+   export default validator;
+
+
+
+
+
+
+    
+  
+    
+  
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+   
+   
+  
+
+
+
+
+
+
+
+
+
+//let multiplicaIndicesPares =  cardNumber.map(function (numero, indice) {
+  //  if (indice % 2 === 0) {
+  //    return numero * 2 ; 
+ //   }
+ //  
+  
+  //  })
+
+//
+//console.log(multiplicaIndicesPares)
+
+
+
+
+
+
+
+
+
+
+//essa nao funcionou:
+//function passandoPelosPares(cardNumber) {
+  //for (var i = 0; i < 16; i++) {
+    //  if ((i % 2) == 0) { i*2
+
+                      
+      //}
+  //}
+//}
+
+//console.log(passandoPelosPares)
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  //let multiplicaIndicesPares =  cardNumber.map(function (numero, indice) {
+ //   if (indice % 2 === 0) {
+ //     return numero * 2; 
+ //   }
+//   return numero;
+ 
+//   })
+//console.log(multiplicaIndicesPares)
+
+ 
+
+
+
+ 
+
+// function maskify(infocard) {
+ // return infocard.slice(0, -4).replace(/[0-9]/g, '#').concat(infocard.slice(-4, infocard.len));
+//}
+//console.log(maskify("12158663221")) 
+//

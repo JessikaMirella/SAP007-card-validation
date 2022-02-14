ab343d12349a23f58c34b81ad9cadf4d3b8c3b51
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-02-14 15:12:33 -0300

    Updates

--- validator.js
+++ validator.js
@@ -5,54 +5,57 @@ const validator = {
   isValid: 
 
   function (cardNumberString) { 
-   console.log(cardNumberString, "string")
-    let cardNumber = cardNumberString.split("")
-    //console.log(cardNumber, "array?")
-
-    cardNumber.reverse();
-   // console.log (cardNumber, "inverteu?")
-    let dobra = 0
-    let sub = 0
-    let total = 0
-
-  for (let i = 0; i < cardNumber.length; i++) {
-  
-      if (i % 2 === 0) {
-  
-          total = total + cardNumber[i]
-        //  console.log("entrou no if",i, cardNumber[i], "essa posição é impar" )
-  
-  
-      } else {
-          dobra = cardNumber[i] * 2;
-        //  console.log("entrou no else",i, cardNumber[i], "essa posição é par", dobra )
-          
-          if (dobra > 9) {
-              sub = dobra - 9
-        //  console.log(" o dobro é maior que 9")
-              total = total + sub
-          }
-  
-          else {
-              total = total + dobra
-          
-  
-          } if (total % 10 === 0 ) { 
+    console.log(cardNumberString, "string")
+     let cardNumber = cardNumberString.split("")
+     console.log(cardNumber, "array?")
+ 
+     cardNumber.reverse();
+     console.log (cardNumber, "inverteu?")
+     let dobra = 0
+     let sub = 0
+     let total = 0
+ 
+   for (let i = 0; i < cardNumber.length; i++) {
+   
+       if (i % 2 === 0) {
+   
+           total = total + cardNumber[i]
+           //  console.log("entrou no if",i, cardNumber[i], "essa posição é impar" )
+            //guardando os impares
+   
+       } else {
+           dobra = cardNumber[i] * 2;
+          console.log("entrou no else",i, cardNumber[i], "essa posição é par", dobra )
+         
+           //ATENÇAO
+           if (dobra > 9) {
+               sub = dobra - 9
+              //  console.log(" o dobro é maior que 9")
+               total = total + sub
+           }
+   
+           else {
+               total = total + dobra + sub
            
-           return true;
-           
-    
-          } else{ 
-           
-
-            return false;
+   
+           } if (total % 10 === 0 ) { 
             
-          }
-  
-  
-      }
-    }
-  },
+            return true;
+            
+     
+           } else{ 
+            
+ 
+             return false;
+             
+           }
+   
+   
+       }
+     }
+   },
+
+ 
 
   maskify:
   
@@ -75,9 +78,7 @@ const validator = {
 
 
 
-    
-  
-    
+ 
   
 
 
@@ -104,16 +105,6 @@ const validator = {
 
 
 
-//let multiplicaIndicesPares =  cardNumber.map(function (numero, indice) {
-  //  if (indice % 2 === 0) {
-  //    return numero * 2 ; 
- //   }
- //  
-  
-  //  })
-
-//
-//console.log(multiplicaIndicesPares)
 
 
 
@@ -124,17 +115,6 @@ const validator = {
 
 
 
-//essa nao funcionou:
-//function passandoPelosPares(cardNumber) {
-  //for (var i = 0; i < 16; i++) {
-    //  if ((i % 2) == 0) { i*2
-
-                      
-      //}
-  //}
-//}
-
-//console.log(passandoPelosPares)
 
 
 
@@ -149,23 +129,10 @@ const validator = {
 
   
 
-  //let multiplicaIndicesPares =  cardNumber.map(function (numero, indice) {
- //   if (indice % 2 === 0) {
- //     return numero * 2; 
- //   }
-//   return numero;
- 
-//   })
-//console.log(multiplicaIndicesPares)
+  
 
  
 
 
 
  
-
-// function maskify(infocard) {
- // return infocard.slice(0, -4).replace(/[0-9]/g, '#').concat(infocard.slice(-4, infocard.len));
-//}
-//console.log(maskify("12158663221")) 
-//

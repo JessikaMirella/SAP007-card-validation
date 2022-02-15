9a18ab6b135d933db3278187221c883e96e8623a
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-02-15 10:19:13 -0300

    Updates

--- validator.js
+++ validator.js
@@ -1,56 +1,35 @@
 
 
 const validator = {
-
-  isValid:
-
-    function (cardNumberString) {
-      console.log(cardNumberString, "string")
-      let cardNumber = cardNumberString.split("")
-      console.log(cardNumber, "array?")
-
-      cardNumber.reverse();
-      console.log(cardNumber, "inverteu?")
-      let dobra = 0
-      let sub = 0
-      let total = 0
-
-      for (let i = 0; i < cardNumber.length; i++) {
-
-        if (i % 2 === 0) {
-
-          total = total + cardNumber[i]
-
-        } else {
-          dobra = cardNumber[i] * 2;
-
-          if (dobra > 9) {
-            sub = dobra - 9
-
-            total = total + sub
-          }
-
-          else {
-
-            total = total + dobra + sub
-
-
-          } if (total % 10 === 0) {
-
-            return true;
-
-
-          } else {
-
-
-            return false;
-
-          }
-
-
+  
+  isValid: function (cardNumberString) {
+    
+    let cardNumber = cardNumberString.split('').reverse();  // split = tranforma string em array 
+    let soma = 0; 
+    
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i % 2 === 1) {
+        const dobro = cardNumber[i] * 2;
+        if (dobro > 9) {
+          soma = soma + (dobro - 9);
+        }
+        else {
+          soma = soma + dobro;
         }
       }
-    },
+      else {
+        soma = soma + parseInt(cardNumber[i]); // transforma em numero porque é soma 
+      }
+    }
+    
+    if (soma % 10 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  
+  },
 
   maskify:
 
@@ -61,9 +40,9 @@ const validator = {
       let first = cardNumber.substr(0, 0);
       let maskingCharacters = cardNumber.substr(1, cardNumber.length - 5).replace(/\d/g, '#');
       return `${first}${maskingCharacters}${last4}`;
-    }
+     }};
 
-};
+  
 
 
 export default validator;

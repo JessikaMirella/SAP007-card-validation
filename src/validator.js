e11b6cf29d9ba9089f97320b7e4981565167a54d
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-02-15 10:18:02 -0300

    aleração apos feedback c/ mentor

--- src/validator.js
+++ src/validator.js
@@ -1,138 +1,112 @@
 
 
 const validator = {
-   
-  isValid: 
-
-  function (cardNumberString) { 
-    console.log(cardNumberString, "string")
-     let cardNumber = cardNumberString.split("")
-     console.log(cardNumber, "array?")
- 
-     cardNumber.reverse();
-     console.log (cardNumber, "inverteu?")
-     let dobra = 0
-     let sub = 0
-     let total = 0
- 
-   for (let i = 0; i < cardNumber.length; i++) {
-   
-       if (i % 2 === 0) {
-   
-           total = total + cardNumber[i]
-           //  console.log("entrou no if",i, cardNumber[i], "essa posição é impar" )
-            //guardando os impares
-   
-       } else {
-           dobra = cardNumber[i] * 2;
-          console.log("entrou no else",i, cardNumber[i], "essa posição é par", dobra )
-         
-           //ATENÇAO
-           if (dobra > 9) {
-               sub = dobra - 9
-              //  console.log(" o dobro é maior que 9")
-               total = total + sub
-           }
-   
-           else {
-               total = total + dobra + sub
-           
-   
-           } if (total % 10 === 0 ) { 
-            
-            return true;
-            
-     
-           } else{ 
-            
- 
-             return false;
-             
-           }
-   
-   
-       }
-     }
-   },
-
- 
+  
+  isValid: function (cardNumberString) {
+    
+    let cardNumber = cardNumberString.split('').reverse();  // split = tranforma string em array 
+    let soma = 0; 
+    
+    for (let i = 0; i < cardNumber.length; i++) {
+      if (i % 2 === 1) {
+        const dobro = cardNumber[i] * 2;
+        if (dobro > 9) {
+          soma = soma + (dobro - 9);
+        }
+        else {
+          soma = soma + dobro;
+        }
+      }
+      else {
+        soma = soma + parseInt(cardNumber[i]); // transforma em numero porque é soma 
+      }
+    }
+    
+    if (soma % 10 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  
+  },
 
   maskify:
-  
+
     function (cardNumber) {
 
-    if (cardNumber.length < 6) return cardNumber;
-    let last4 = cardNumber.substr(-4);
-    let first = cardNumber.substr(0, 0);
-    let maskingCharacters = cardNumber.substr(1, cardNumber.length - 5).replace(/\d/g, '#');
-   return  `${first}${maskingCharacters}${last4}` ;
-  }
-   
-  };
-  
-
-   export default validator;
-
-
-
-
-
-
- 
-  
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-   
-   
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      if (cardNumber.length < 6) return cardNumber;
+      let last4 = cardNumber.substr(-4);
+      let first = cardNumber.substr(0, 0);
+      let maskingCharacters = cardNumber.substr(1, cardNumber.length - 5).replace(/\d/g, '#');
+      return `${first}${maskingCharacters}${last4}`;
+     }};
 
   
 
-  
 
- 
-
+export default validator;
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
